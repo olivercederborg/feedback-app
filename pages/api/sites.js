d8adfe8572f56a861b4180100b65a5f7892e6c70
Oliver Cederborg <hi@olivercederborg.com>
2021-02-05 00:31:53 +0100

    getStaticPaths fix

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -5,7 +5,7 @@ export default async (req, res) => {
   try {
     const { uid } = await auth.verifyIdToken(req.headers.token);
     const { sites } = await getUserSites(uid);
-    // console.log(user);
+
     res.status(200).json({ sites });
   } catch (error) {
     res.status(500).json({ error });
